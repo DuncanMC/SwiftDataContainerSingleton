c1970cabeffccef858549f1b3bfb2b40ce2e92aa
Duncan Champney <DuncanC@aol.com>
2015-04-19 11:44:10 -0400

    Initial Commit

---
+++ SwiftDataContainerSingleton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDataContainerSingleton
+//
+//  Created by Duncan Champney on 4/19/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
